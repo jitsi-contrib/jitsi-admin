36297176108a0fb5ea293ffc20d6ee57ca81add7
Entwicklung <entwicklung@h2-invent.com>
2023-04-21 21:45:42 +0200

    add docker installation to installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -10,7 +10,7 @@ EOF
 echo Welcome to the installer:
 
 sudo apt-get update
-sudo apt-get install \
+sudo apt-get -y install \
     ca-certificates \
     curl \
     gnupg
@@ -23,7 +23,7 @@ echo \
 sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin curl
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin curl
 curl -SL https://github.com/docker/compose/releases/download/v2.7.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 chmod +x  /usr/local/bin/docker-compose
 docker-compose -v
