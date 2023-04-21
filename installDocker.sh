25bb220821170f181828f024bc3ee1e360f02729
Entwicklung <entwicklung@h2-invent.com>
2023-04-21 21:46:12 +0200

    add docker installation to installDocker.sh

--- installDocker.sh
+++ installDocker.sh
@@ -10,10 +10,7 @@ EOF
 echo Welcome to the installer:
 
 sudo apt-get update
-sudo apt-get -y install \
-    ca-certificates \
-    curl \
-    gnupg
+sudo apt-get -y install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
